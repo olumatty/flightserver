79eb59557277cedee085c20cd3b3064f70eb62a7
olumatty <olumatty@gmail.com>
2025-04-27 04:06:01 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+}));
+
 
 let accessToken = null;
 let tokenExpiration = 0;
